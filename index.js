8c5e55648a9760daab540331fc79a1a5bc667f5b
aljagne <ajagne01@gmail.com>
2022-03-31 14:10:00 +0000

    finished the login and the logout section with the api

--- index.js
+++ index.js
@@ -1,148 +1,111 @@
-require('dotenv').config()
+require("dotenv").config()
 const express = require("express")
-const querystring = require('querystring')
+const querystring = require("querystring")
 const app = express()
-const axios = require('axios')
+const axios = require("axios")
 const port = 8888
 
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI
 
-
-console.log(process.env.CLIENT_ID)
-//app
-
-app.get("/", (req, res) => {
-  const data = {
-    name: 'Badou',
-    isAwesome: true
-  }
-  res.json(data)
-})
-
-
-
-
-app.get('/awesome-generator', (req, res) => {
-  const { name, isAwesome } = req.query;
-  res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
-});
-
-
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-const generateRandomString = length => {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const generateRandomString = (length) => {
+  let text = ""
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
   for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
   }
-  return text;
-};
+  return text
+}
 
+const stateKey = "spotify_auth_state"
 
-const stateKey = 'spotify_auth_state';
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16)
+  res.cookie(stateKey, state)
 
-app.get('/login', (req, res) => {
-  const state = generateRandomString(16);
-  res.cookie(stateKey, state);
-
-  const scope = 'user-read-private user-read-email';
+  const scope = "user-read-private user-read-email"
 
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
-    response_type: 'code',
+    response_type: "code",
     redirect_uri: REDIRECT_URI,
     state: state,
     scope: scope,
-  });
+  })
 
-  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
-});
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`)
+})
 
-
-app.get('/callback', (req, res) => {
-  const code = req.query.code || null;
+app.get("/callback", (req, res) => {
+  const code = req.query.code || null
 
   axios({
-    method: 'post',
-    url: 'https://accounts.spotify.com/api/token',
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
     data: querystring.stringify({
-      grant_type: 'authorization_code',
+      grant_type: "authorization_code",
       code: code,
-      redirect_uri: REDIRECT_URI
+      redirect_uri: REDIRECT_URI,
     }),
     headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
     },
   })
-    .then(response => {
+    .then((response) => {
       if (response.status === 200) {
+        const { access_token, refresh_token, expires_in } = response.data
 
-        // const { access_token, token_type } = response.data;
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+          expires_in,
+        })
 
-        // axios.get('https://api.spotify.com/v1/me', {
-        //   headers: {
-        //     Authorization: `${token_type} ${access_token}`
-        //   }
-        // })
-        //   .then(response => {
-        //     res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-        //   })
-        //   .catch(error => {
-        //     res.send(error);
-        //   });
-
-
-        const { refresh_token } = response.data;
-
-        axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
-          .then(response => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch(error => {
-            res.send(error);
-          });
-
+        res.redirect(`http://172.19.165.90:3000/?${queryParams}`)
       } else {
-        res.send(response);
+        res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`)
       }
     })
-    .catch(error => {
-      res.send(error);
-    });
-});
+    .catch((error) => {
+      res.send(error)
+    })
+})
 
-
-app.get('/refresh_token', (req, res) => {
-  const { refresh_token } = req.query;
+app.get("/refresh_token", (req, res) => {
+  const { refresh_token } = req.query
 
   axios({
-    method: 'post',
-    url: 'https://accounts.spotify.com/api/token',
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
     data: querystring.stringify({
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
     }),
     headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
     },
   })
-    .then(response => {
-      res.send(response.data);
+    .then((response) => {
+      res.send(response.data)
     })
-    .catch(error => {
-      res.send(error);
-    });
-});
-
+    .catch((error) => {
+      res.send(error)
+    })
+})
 
 app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
-});
+  console.log(`Express app listening at http://localhost:${port}`)
+})
