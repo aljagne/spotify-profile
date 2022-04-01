7aa007b85667f7914d0ffeb24ed9ec56e45bf509
aljagne <ajagne01@gmail.com>
2022-04-01 12:58:02 +0000

    not finish yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
-import "./App.css"
-import { useEffect, useState } from "react"
+import { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  useLocation
-} from "react-router-dom"
-import { accessToken, logout, getCurrentUserProfile } from "./spotify"
-import { catchErrors } from "./utils"
+  useLocation,
+} from 'react-router-dom';
+import { accessToken, logout } from './spotify';
+import { Login, Profile, TopArtists } from './pages';
+import { GlobalStyle } from './styles';
+import styled from 'styled-components/macro';
 
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
 function ScrollToTop() {
   const { pathname } = useLocation();
 
@@ -20,34 +39,28 @@ function ScrollToTop() {
 }
 
 function App() {
-  const [token, setToken] = useState(null)
-  const [profile, setProfile] = useState(null)
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    setToken(accessToken)
-
-    const fetchData = async () => {
-      const { data } = await getCurrentUserProfile()
-      setProfile(data)
-    }
-
-    catchErrors(fetchData())
-  }, [])
+    setToken(accessToken);
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {!token ? (
-          <a className="App-link" href="http://localhost:8888/login">
-            Log in to Spotify
-          </a>
-        ) : (
+    <div className="app">
+      <GlobalStyle />
+
+      {!token ? (
+        <Login />
+      ) : (
+        <>
+          <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+
           <Router>
             <ScrollToTop />
 
             <Switch>
               <Route path="/top-artists">
-                <h1>Top Artists</h1>
+                <TopArtists />
               </Route>
               <Route path="/top-tracks">
                 <h1>Top Tracks</h1>
@@ -59,26 +72,14 @@ function App() {
                 <h1>Playlists</h1>
               </Route>
               <Route path="/">
-                <>
-                  <button onClick={logout}>Log Out</button>
-
-                  {profile && (
-                    <div>
-                      <h1>{profile.display_name}</h1>
-                      <p>{profile.followers.total} Followers</p>
-                      {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar"/>
-                      )}
-                    </div>
-                  )}
-                </>
+                <Profile />
               </Route>
             </Switch>
           </Router>
-        )}
-      </header>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- index.js
+++ index.js
@@ -1,111 +1,118 @@
-require("dotenv").config()
-const express = require("express")
-const querystring = require("querystring")
-const app = express()
-const axios = require("axios")
-const port = 8888
+require('dotenv').config();
 
-const CLIENT_ID = process.env.CLIENT_ID
-const CLIENT_SECRET = process.env.CLIENT_SECRET
-const REDIRECT_URI = process.env.REDIRECT_URI
+const express = require('express');
+const querystring = require('querystring');
+const axios = require('axios');
+
+const app = express();
+const port = 8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-const generateRandomString = (length) => {
-  let text = ""
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return text
-}
+  return text;
+};
 
-const stateKey = "spotify_auth_state"
 
-app.get("/login", (req, res) => {
-  const state = generateRandomString(16)
-  res.cookie(stateKey, state)
+const stateKey = 'spotify_auth_state';
 
-  const scope = "user-read-private user-read-email"
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+  ].join(' ');
 
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
-    response_type: "code",
+    response_type: 'code',
     redirect_uri: REDIRECT_URI,
     state: state,
     scope: scope,
-  })
+  });
 
-  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`)
-})
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
 
-app.get("/callback", (req, res) => {
-  const code = req.query.code || null
+
+app.get('/callback', (req, res) => {
+  const code = req.query.code || null;
 
   axios({
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
     data: querystring.stringify({
-      grant_type: "authorization_code",
+      grant_type: 'authorization_code',
       code: code,
-      redirect_uri: REDIRECT_URI,
+      redirect_uri: REDIRECT_URI
     }),
     headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${new Buffer.from(
-        `${CLIENT_ID}:${CLIENT_SECRET}`
-      ).toString("base64")}`,
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
     },
   })
-    .then((response) => {
+    .then(response => {
       if (response.status === 200) {
-        const { access_token, refresh_token, expires_in } = response.data
+        const { access_token, refresh_token, expires_in } = response.data;
 
         const queryParams = querystring.stringify({
           access_token,
           refresh_token,
           expires_in,
-        })
+        });
 
-        res.redirect(`http://172.19.165.90:3000/?${queryParams}`)
+        res.redirect(`http://localhost:3000/?${queryParams}`);
+
       } else {
-        res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`)
+        res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
       }
     })
-    .catch((error) => {
-      res.send(error)
-    })
-})
+    .catch(error => {
+      res.send(error);
+    });
+});
 
-app.get("/refresh_token", (req, res) => {
-  const { refresh_token } = req.query
+
+app.get('/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
 
   axios({
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
     data: querystring.stringify({
-      grant_type: "refresh_token",
-      refresh_token: refresh_token,
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
     }),
     headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${new Buffer.from(
-        `${CLIENT_ID}:${CLIENT_SECRET}`
-      ).toString("base64")}`,
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
     },
   })
-    .then((response) => {
-      res.send(response.data)
+    .then(response => {
+      res.send(response.data);
     })
-    .catch((error) => {
-      res.send(error)
-    })
-})
+    .catch(error => {
+      res.send(error);
+    });
+});
+
 
 app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`)
-})
+  console.log(`Express app listening at http://localhost:${port}`);
+});
